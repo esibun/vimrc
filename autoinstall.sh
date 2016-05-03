445b926bdbd108a0211fdb81918a0319dff80c79
esi <xpmaster2k4@gmail.com>
2016-05-03 14:50:37 -0400

    add autoinstaller

---
+++ autoinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd ~/.vim
+git submodule update --init --recursive
+mkdir ycm_build
+cd ycm_build
+cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+cmake --build . --target ycm_core --config Release
+cd ~/.vim
+rm -rf ycm_build
