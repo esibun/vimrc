b282042a392c52d92b79227955944b16ed612a5c
esi <git@esibun.net>
2018-12-28 22:09:30 -0800

    Modernize the install script

--- autoinstall.sh
+++ autoinstall.sh
@@ -3,9 +3,5 @@
 cd ~/.vim
 git submodule update --init --recursive
 mkdir -p backups
-mkdir ycm_build
-cd ycm_build
-cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
-cmake --build . --target ycm_core --config Release
-cd ~/.vim
-rm -rf ycm_build
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
